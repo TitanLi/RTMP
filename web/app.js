19fa7255fb821352aeeba1edf0a208cab43f87a8
TitanLi <Lisheng0706@gmail.com>
2018-06-12 15:23:35 +0800

    Add RTMP

---
+++ web/app.js
@@ -0,0 +1,36 @@
+//sudo apt install ffmpeg
+const koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const co = require('co');
+const render = require('koa-swig');
+const dotenv = require('dotenv').load();
+
+const app = new koa();
+const router = Router();
+
+app.use(logger());
+app.use(bodyParser());
+app.use(serve(__dirname+'/views'));
+app.use(router.routes());
+
+app.context.render = co.wrap(render({
+  root: __dirname + '/views',
+  autoescape: true,
+  cache: 'memory',
+  ext: 'html',
+}));
+
+app.use(logger());
+app.use(serve('./views'));
+app.use(serve('./public'));
+
+router.get('/',home);
+async function home(ctx){
+  ctx.body = await ctx.render('home',{data:'Hello RTMP'});
+}
+
+app.listen(3000);
+console.log('listening on port 3000');
